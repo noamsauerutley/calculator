f0db5bd89b641a419fd83c8d696cfd4e246b118f
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-05 14:13:39 -0500

    Add factorial method + test)

--- calculator.rb
+++ calculator.rb
@@ -7,6 +7,14 @@ class Calculator
   def add(a, b)
     a + b
   end
+
+  def factorial(n)
+    if n < 1
+      1
+    else
+      n * factorial(n - 1)
+    end
+  end
 end
 
 # the string below is purely documentation
@@ -20,7 +28,21 @@ describe Calculator do
     end
 
     it "returns the sum of two different arguments" do
+      calc = Calculator.new
+      expect(calc.add(1, 2)).to eq(3)
+    end
+  end
 
+  describe '#factorial' do
+    it 'returns 1 when given 0' do
+      calc = Calculator.new
+
+      expect(calc.factorial(0)).to eq(1)
+    end
+    it 'returns 120 when fiven 5' do
+      calc = Calculator.new
+
+      expect(calc.factorial(5)).to eq(120)
     end
   end
 end
